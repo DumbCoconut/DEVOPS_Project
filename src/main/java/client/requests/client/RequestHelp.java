41b0c3c9cf11c0d8f08f41765ad28418f797b516
Legras Mathieu <legrasmathieu0@gmail.com>
2017-04-09 22:05:46 +0200

    Experimental levenshtein distance...

--- src/main/java/client/requests/client/RequestHelp.java
+++ src/main/java/client/requests/client/RequestHelp.java
@@ -4,7 +4,10 @@ import client.requests.Request;
 import client.requests.RequestName;
 import client.requests.exceptions.NoTokensException;
 
-import java.util.ArrayList;
+import java.util.*;
+import java.util.Map.Entry;
+
+import static org.apache.commons.lang3.StringUtils.getLevenshteinDistance;
 
 public class RequestHelp extends Request {
     ArrayList<String> requests;
@@ -31,22 +34,68 @@ public class RequestHelp extends Request {
         } else {
             String res = "";
             for (String cmd : requests) {
-                if (cmd.equals(RequestName.HELP))               res += helpHelp() + "\n";
-                else if (cmd.equals(RequestName.QUIT))          res += helpQuit() + "\n";
-                else if (cmd.equals(RequestName.ADD_SERVER))    res += helpAddServer() + "\n";
-                else if (cmd.equals(RequestName.GET))           res += helpGet() + "\n";
-                else if (cmd.equals(RequestName.SET))           res += helpSet() + "\n";
-                else if (cmd.equals(RequestName.DECR))          res += helpDecr() + "\n";
-                else if (cmd.equals(RequestName.DECRBY))        res += helpDecrBy() + "\n";
-                else if (cmd.equals(RequestName.INCR))          res += helpIncr() + "\n";
-                else if (cmd.equals(RequestName.INCRBY))        res += helpIncrBy() + "\n";
-                else if (cmd.equals(RequestName.TYPE))          res += helpType() + "\n";
-                else if (cmd.equals(RequestName.DEL))           res += helpDel() + "\n";
-                else                                            res += "(error) I'm sorry, I don't recognize "
-                                                                            + "the command \"" + cmd + "\"." + "\n";
+                switch (cmd) {
+                    case RequestName.HELP:
+                        res += helpHelp() + "\n";
+                        break;
+                    case RequestName.QUIT:
+                    case RequestName.EXIT:
+                        res += helpQuit() + "\n";
+                        break;
+                    case RequestName.ADD_SERVER:
+                        res += helpAddServer() + "\n";
+                        break;
+                    case RequestName.GET:
+                        res += helpGet() + "\n";
+                        break;
+                    case RequestName.SET:
+                        res += helpSet() + "\n";
+                        break;
+                    case RequestName.DECR:
+                        res += helpDecr() + "\n";
+                        break;
+                    case RequestName.DECRBY:
+                        res += helpDecrBy() + "\n";
+                        break;
+                    case RequestName.INCR:
+                        res += helpIncr() + "\n";
+                        break;
+                    case RequestName.INCRBY:
+                        res += helpIncrBy() + "\n";
+                        break;
+                    case RequestName.TYPE:
+                        res += helpType() + "\n";
+                        break;
+                    case RequestName.DEL:
+                        res += helpDel() + "\n";
+                        break;
+                    default:
+                        res += "(error) I'm sorry, I don't recognize the command \"" + cmd + "\". "
+                                + "Did you mean \"" + findClosestCmdMatch(cmd) + "\"?";
+                        break;
+                }
             }
             return res;
         }
+    }
+
+    private String findClosestCmdMatch(String cmd) {
+        HashMap<String, Integer> distances = new HashMap<>();
+        distances.put(RequestName.HELP, getLevenshteinDistance(cmd, RequestName.HELP));
+        distances.put(RequestName.QUIT, getLevenshteinDistance(cmd, RequestName.QUIT));
+        distances.put(RequestName.ADD_SERVER, getLevenshteinDistance(cmd, RequestName.ADD_SERVER));
+        distances.put(RequestName.GET, getLevenshteinDistance(cmd, RequestName.GET));
+        distances.put(RequestName.SET, getLevenshteinDistance(cmd, RequestName.SET));
+        distances.put(RequestName.DECR, getLevenshteinDistance(cmd, RequestName.DECR));
+        distances.put(RequestName.DECRBY, getLevenshteinDistance(cmd, RequestName.DECRBY));
+        distances.put(RequestName.INCR, getLevenshteinDistance(cmd, RequestName.INCR));
+        distances.put(RequestName.INCRBY, getLevenshteinDistance(cmd, RequestName.INCRBY));
+        distances.put(RequestName.TYPE, getLevenshteinDistance(cmd, RequestName.TYPE));
+        distances.put(RequestName.DEL, getLevenshteinDistance(cmd, RequestName.DEL));
+        Object[] entrySet = distances.entrySet().toArray();
+        Arrays.sort(entrySet, (o1, o2) -> ((Entry<String, Integer>) o2).getValue()
+                .compareTo(((Entry<String, Integer>) o1).getValue()));
+        return ((Entry<String, Integer>) entrySet[entrySet.length - 1]).getKey();
     }
 
     private String help() {

eea3995b0b568706132fd4e72c2512505338c645
Legras Mathieu <legrasmathieu0@gmail.com>
2017-04-14 20:23:27 +0200

    Tests storage exceptions

--- src/test/java/storage/StorageTestSuite.java
+++ src/test/java/storage/StorageTestSuite.java
@@ -2,9 +2,11 @@ package storage;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+import storage.exceptions.ExceptionsStorageTestSuite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        StorageTest.class
+        StorageTest.class,
+        ExceptionsStorageTestSuite.class
 })
 public class StorageTestSuite {}
---
+++ src/test/java/storage/exceptions/NonExistentKeyExceptionTest.java
@@ -0,0 +1,19 @@
+package storage.exceptions;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NonExistentKeyExceptionTest {
+    NonExistentKeyException e;
+
+    @Before
+    public void init() {
+        e = new NonExistentKeyException("poupi");
+    }
+    @Test
+    public void getMessage() throws Exception {
+        assertEquals("The key \"poupi\" does not exist in the cache.", e.getMessage());
+    }
+}

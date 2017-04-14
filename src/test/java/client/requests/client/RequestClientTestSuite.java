fbb4fd2cc27eff5096a5b388f21f4c086f60da7f
Legras Mathieu <legrasmathieu0@gmail.com>
2017-04-14 18:18:27 +0200

    RequestHelpTest added to the test suite

--- src/test/java/client/requests/client/RequestClientTestSuite.java
+++ src/test/java/client/requests/client/RequestClientTestSuite.java
@@ -5,6 +5,7 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        RequestAddServerTest.class
+        RequestAddServerTest.class,
+        RequestHelpTest.class
 })
 public class RequestClientTestSuite {}

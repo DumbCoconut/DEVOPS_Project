b779edbe8dd9ff4cb86d1d63fb0bafaefb9f30c8
Legras Mathieu <legrasmathieu0@gmail.com>
2017-04-17 20:59:40 +0200

    Removed doc as it is not up to date

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -440,21 +440,6 @@ public class Client {
         System.out.println("Server is not set. Please add a server. Type \"help add_server\" if you need help.");
     }
 
-    /**
-     * Test if the specified key exists.
-     * @param key The key to test.
-     * @return True if the key exists, false otherwise.
-     */
-    private boolean exists(String key) {
-        // TODO
-        return false;
-    }
-
-    /**
-     * Get the value of the specified key.
-     * @param key The key we want the value of.
-     * @return The value of the key if it exists, null otherwise.
-     */
     private Object get(String key) {
         try {
             return server.get(key);
@@ -464,11 +449,6 @@ public class Client {
         }
     }
 
-    /**
-     * Set key to hold the string value. If key already holds a value, it is overwritten, regardless of its type.
-     * @param key The key holding the value.
-     * @param value The value to set.
-     */
     private void set(String key, Object value) {
         try {
             server.set(key, value);
@@ -477,11 +457,6 @@ public class Client {
         }
     }
 
-    /**
-     * Return the type of the value stored at key in form of a string.
-     * @param key The key holding the value.
-     * @return one of "none", "string", "int", "list". "none" is returned if the key does not exist.
-     */
     private String type(String key) {
         try {
             return server.type(key);
@@ -491,15 +466,6 @@ public class Client {
         }
     }
 
-    /**
-     * Decrement the number stored at key by one.
-     * <p>
-     *     If the key does not exist or contains a value of a wrong type, set the key to the value of "0"
-     *     before to perform the increment or decrement operation.
-     * </p>
-     * @param key The key holding the value.
-     * @return the new value of key after the decrement.
-     */
     private int decr(String key) {
         try {
             return server.decr(key);
@@ -509,16 +475,6 @@ public class Client {
         }
     }
 
-    /**
-     * Decrement the number stored at key by one.
-     * <p>
-     *     If the key does not exist or contains a value of a wrong type, set the key to the value of "0"
-     *     before to perform the increment or decrement operation.
-     * </p>
-     * @param key The key holding the value.
-     * @param integer The increment value.
-     * @return the new value of key after the decrement.
-     */
     private int decrBy(String key, int integer) {
         try {
             return server.decrBy(key, integer);
@@ -528,15 +484,6 @@ public class Client {
         }
     }
 
-    /**
-     * Increment the number stored at key by one.
-     * <p>
-     *     If the key does not exist or contains a value of a wrong type, set the key to the value of "0"
-     *     before to perform the increment or decrement operation.
-     * </p>
-     * @param key The key holding the value.
-     * @return the new value of key after the Increment.
-     */
     private int incr(String key) {
         try {
             return server.incr(key);
@@ -546,16 +493,6 @@ public class Client {
         }
     }
 
-    /**
-     * Increment the number stored at key by one.
-     * <p>
-     *     If the key does not exist or contains a value of a wrong type, set the key to the value of "0"
-     *     before to perform the increment or decrement operation.
-     * </p>
-     * @param key The key holding the value.
-     * @param integer The increment value.
-     * @return the new value of key after the Increment.
-     */
     private int incrBy(String key, int integer) {
         try {
             return server.incrBy(key, integer);
@@ -565,11 +502,6 @@ public class Client {
         }
     }
 
-    /**
-     * Remove the specified key. If a given key does not exist no operation is performed for this key.
-     * @param key The key to remove.
-     * @return True if the key existed and has been removed, false otherwise.
-     */
     private boolean del(String key) {
         try {
             return server.del(key);
